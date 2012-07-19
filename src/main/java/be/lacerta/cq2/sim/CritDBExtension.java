995186130c1659c7c1c4f41d406025521e471b89
Looneytje <hr.1989@gmail.com>
2012-07-19 20:15:43 +0200

    Fixed possible nullpointerexceptions.

--- src/main/java/be/lacerta/cq2/sim/CritDBExtension.java
+++ src/main/java/be/lacerta/cq2/sim/CritDBExtension.java
@@ -1,6 +1,5 @@
 package be.lacerta.cq2.sim;
 
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -20,29 +19,37 @@ public class CritDBExtension extends AbstractSimExtension {
 		String action = request.getParameter("action");
 		request.setAttribute("action", action);
 		request.setAttribute("result", " ");
-		
+
 		if (action == null || action.equals("")) {
 			if (StringUtils.isInteger(request.getParameter("race"))) {
-				request.setAttribute("critdb_creatures", Creature.getCreatures(request));
+				request.setAttribute("critdb_creatures",
+						Creature.getCreatures(request));
 				setPath("/critdb.jsp");
 			} else {
 				setPath("/main.jsp");
 			}
-		} 
-		else if (action.equals("search")) doSearch();
-		else if (action.equals("critcalc")) doCritCalc();
-		else if (action.equals("critlist")) doCritList();
-		else if (action.equals("itemlist")) doItemList();
-		else if (action.equals("costcalc")) doCostCalc();
-		else if (action.equals("levelcalc")) doMaxLevelCalc();
-		else if (action.equals("summoncalc")) doSummonChanceCalc();
-		else setPath("/main.jsp");
-		
+		} else if (action.equals("search"))
+			doSearch();
+		else if (action.equals("critcalc"))
+			doCritCalc();
+		else if (action.equals("critlist"))
+			doCritList();
+		else if (action.equals("itemlist"))
+			doItemList();
+		else if (action.equals("costcalc"))
+			doCostCalc();
+		else if (action.equals("levelcalc"))
+			doMaxLevelCalc();
+		else if (action.equals("summoncalc"))
+			doSummonChanceCalc();
+		else
+			setPath("/main.jsp");
+
 	}
 
-	private void doCritCalc() {	
-        // $DY = ($_GET['DX'] * (0.8 + 0.2*$_GET['LY']))/(0.8+0.2*$_GET['LX']);
-        // $HY = ($_GET['HX'] * (0.8 + 0.2*$_GET['LY']))/(0.8+0.2*$_GET['LX']);
+	private void doCritCalc() {
+		// $DY = ($_GET['DX'] * (0.8 + 0.2*$_GET['LY']))/(0.8+0.2*$_GET['LX']);
+		// $HY = ($_GET['HX'] * (0.8 + 0.2*$_GET['LY']))/(0.8+0.2*$_GET['LX']);
 
 		if (post) {
 			try {
@@ -50,20 +57,24 @@ public class CritDBExtension extends AbstractSimExtension {
 				request.setAttribute("critdb_DX", request.getParameter("DX"));
 				request.setAttribute("critdb_HX", request.getParameter("HX"));
 				request.setAttribute("critdb_LY", request.getParameter("LY"));
-				
+
 				request.setAttribute("critdb_DY",
-						be.lacerta.cq2.battlecalc.objects.Creature.convertStrength(
-								Integer.parseInt(request.getParameter("DX")), 
-								Integer.parseInt(request.getParameter("LX")),
-								Integer.parseInt(request.getParameter("LY"))));
-				
+						be.lacerta.cq2.battlecalc.objects.Creature
+								.convertStrength(Integer.parseInt(request
+										.getParameter("DX")), Integer
+										.parseInt(request.getParameter("LX")),
+										Integer.parseInt(request
+												.getParameter("LY"))));
+
 				request.setAttribute("critdb_HY",
-						be.lacerta.cq2.battlecalc.objects.Creature.convertStrength(
-								Integer.parseInt(request.getParameter("HX")), 
-								Integer.parseInt(request.getParameter("LX")),
-								Integer.parseInt(request.getParameter("LY"))));
+						be.lacerta.cq2.battlecalc.objects.Creature
+								.convertStrength(Integer.parseInt(request
+										.getParameter("HX")), Integer
+										.parseInt(request.getParameter("LX")),
+										Integer.parseInt(request
+												.getParameter("LY"))));
 			} catch (NumberFormatException nfe) {
-				request.setAttribute("critdb_DY",null);
+				request.setAttribute("critdb_DY", null);
 			}
 		} else {
 			request.setAttribute("critdb_DX", "");
@@ -73,80 +84,87 @@ public class CritDBExtension extends AbstractSimExtension {
 		}
 		setPath("/critcalc.jsp");
 	}
-	
-	private void doCritList() {
-		String[] classes = {"Air","Death","Earth","Forest"};
 
-		for (int i=0;i<classes.length; i++) {
-			List<Creature> crits = Creature.getCreatures(classes[i],("show").equals(request.getParameter("all")));
-			request.setAttribute("critdb_"+classes[i]+"Crits",crits);
+	private void doCritList() {
+		String[] classes = { "Air", "Death", "Earth", "Forest" };
+
+		for (int i = 0; i < classes.length; i++) {
+			List<Creature> crits = Creature.getCreatures(classes[i],
+					("show").equals(request.getParameter("all")));
+			request.setAttribute("critdb_" + classes[i] + "Crits", crits);
 		}
 
 		setPath("/critlist.jsp");
 	}
-	
-	private void doItemList() {
-		String[] classes = {"Air","Death","Earth","Forest"};
 
-		for (int i=0;i<classes.length; i++) {
-			List<Item> items = Item.getItems(classes[i],("show").equals(request.getParameter("all")));
-			request.setAttribute("critdb_"+classes[i]+"Items",items);
+	private void doItemList() {
+		String[] classes = { "Air", "Death", "Earth", "Forest" };
+
+		for (int i = 0; i < classes.length; i++) {
+			List<Item> items = Item.getItems(classes[i],
+					("show").equals(request.getParameter("all")));
+			request.setAttribute("critdb_" + classes[i] + "Items", items);
 		}
-		
+
 		setPath("/itemlist.jsp");
 	}
-	
+
 	private void doSearch() {
 		if (post) {
-			request.setAttribute("critdb_creatures", Creature.getCreatures(request));
+			request.setAttribute("critdb_creatures",
+					Creature.getCreatures(request));
 		}
-		setPath("/critdb.jsp");		
+		setPath("/critdb.jsp");
 	}
-	
+
 	private void doCostCalc() {
 		if (post) {
 			// amulets
-			HashMap<Amulet,Integer> amuPrices = new HashMap<Amulet, Integer>();
-			int i =0;
-			while (i<6 && !getString("ammy"+i).equals("")) {
-				int amount = Math.max(1, getInt("ammyamount"+i));
-				Amulet amu = CreatureFinder.findAmulet(request.getParameter("ammy"+i).trim());
-				if (amu!=null) amuPrices.put(amu, amount);
+			HashMap<Amulet, Integer> amuPrices = new HashMap<Amulet, Integer>();
+			int i = 0;
+			while (i < 6 && !getString("ammy" + i).equals("")) {
+				int amount = Math.max(1, getInt("ammyamount" + i));
+				Amulet amu = CreatureFinder.findAmulet(request.getParameter(
+						"ammy" + i).trim());
+				if (amu != null)
+					amuPrices.put(amu, amount);
 				i++;
 			}
-			
-			
+
 			// enchants
 			List<int[]> enchantPrices = new ArrayList<int[]>();
-			i=0;
-			while (i<6 && !getString("level"+i).equals("")) {
-				int amount = Math.max(1, getInt("enchamount"+i));
-				int level = getInt("level"+i); 
+			i = 0;
+			while (i < 6 && !getString("level" + i).equals("")) {
+				int amount = Math.max(1, getInt("enchamount" + i));
+				int level = getInt("level" + i);
 
-				if (level>0) {
+				if (level > 0) {
 					int[] enchant = new int[7];
-					enchant[0] = getInt("enchant"+i); 
+					enchant[0] = getInt("enchant" + i);
 					enchant[1] = level;
 					enchant[2] = amount;
-					enchant[3] = level*getInt("brim"+i);
-					enchant[4] = level*getInt("crys"+i);
-					enchant[5] = level*getInt("ess"+i);
-					enchant[6] = level*getInt("gran"+i);
+					enchant[3] = level * getInt("brim" + i);
+					enchant[4] = level * getInt("crys" + i);
+					enchant[5] = level * getInt("ess" + i);
+					enchant[6] = level * getInt("gran" + i);
 					enchantPrices.add(enchant);
 				}
 				i++;
 			}
-			
-			if (amuPrices.size() > 0) request.setAttribute("costcalc_amuprices", amuPrices);
-			if (enchantPrices.size() > 0) request.setAttribute("costcalc_enchantprices", enchantPrices);
+
+			if (amuPrices.size() > 0)
+				request.setAttribute("costcalc_amuprices", amuPrices);
+			if (enchantPrices.size() > 0)
+				request.setAttribute("costcalc_enchantprices", enchantPrices);
 		}
-		request.setAttribute("costcalc_MIenchanters", MageSkill.loadBySkill(Skill.loadByText("Major Immunity")));
-		request.setAttribute("costcalc_MBenchanters", MageSkill.loadBySkill(Skill.loadByText("Major Berserk")));
+		request.setAttribute("costcalc_MIenchanters",
+				MageSkill.loadBySkill(Skill.loadByText("Major Immunity")));
+		request.setAttribute("costcalc_MBenchanters",
+				MageSkill.loadBySkill(Skill.loadByText("Major Berserk")));
 		request.setAttribute("costcalc_items", Item.getItems("name"));
 		setPath("/costcalc.jsp");
 	}
-	
-	
+
 	private void doSummonChanceCalc() {
 		request.setAttribute("critdb_mageSkill", "");
 		request.setAttribute("critdb_critSkill", "");
@@ -155,30 +173,43 @@ public class CritDBExtension extends AbstractSimExtension {
 			int critSkill = getInt("crit");
 			request.setAttribute("critdb_mageSkill", mageSkill);
 			request.setAttribute("critdb_critSkill", critSkill);
-			request.setAttribute("critdb_chance", CQ2Functions.calcSummonChance(mageSkill,critSkill));
+			request.setAttribute("critdb_chance",
+					CQ2Functions.calcSummonChance(mageSkill, critSkill));
 		}
 		setPath("/critcalc.jsp");
 	}
-	
-	public void doMaxLevelCalc(){
+
+	public void doMaxLevelCalc() {
 		double classMultiplier;
 		int userLevel = 1;
-		double compareValue = userLevel*5.5 -60;
+		double compareValue = userLevel * 5.5 - 60;
+
+		Amulet a = CreatureFinder.findAmulet(request.getParameter("ammy").trim());
 		
-		if(request.getParameter("class").equals("Main class")){
-			classMultiplier = 1;
-		}
-		else classMultiplier = 1.5;
-		
-		Creature crit = Creature.getCreature(request.getParameter("ammy"));
-	
-		while (crit.getSkill()*classMultiplier>compareValue){
-			userLevel++;
-			compareValue = userLevel*5.5 -60;
+		if (a != null && a.getEss() == 0) {
+			
+			if (request.getParameter("class").equals("Main class")) {
+				classMultiplier = 1;
+			} else
+				classMultiplier = 1.5;
+
+			Creature crit = Creature.getCreature(CreatureFinder.findAmulet(
+					request.getParameter("ammy").trim()).getName());
+
+			while (crit.getSkill() * classMultiplier > compareValue) {
+				userLevel++;
+				compareValue = userLevel * 5.5 - 60;
+			}
+
+			request.setAttribute("result",
+					"The maximum magelevel to use a(n) " + crit.getName()
+							+ ", in your "
+							+ request.getParameter("class").toLowerCase()
+							+ ", is " + (userLevel - 1) + ".");
+		} else {
+			request.setAttribute("result", "Enter a correct critname please.");
 		}
 
-		request.setAttribute("result", "The maximum magelevel to use a(n) " + crit.getName() + ", in your " + request.getParameter("class").toLowerCase() + ", is " + (userLevel-1) + ".");
-		
 		setPath("/critcalc.jsp");
 	}
 }
